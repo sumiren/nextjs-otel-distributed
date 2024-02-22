d6023e7b537e2e455620fec8a64846956f350df6
sumiren <sumiren.and.t@gmail.com>
2024-02-22 12:11:34 +0900

    o11y: improve instrumentations

--- backend/src/instrumentation.node.ts
+++ backend/src/instrumentation.node.ts
@@ -2,10 +2,13 @@ import {NodeSDK} from '@opentelemetry/sdk-node'
 import {OTLPTraceExporter} from '@opentelemetry/exporter-trace-otlp-http'
 import {Resource} from '@opentelemetry/resources'
 import {SemanticResourceAttributes} from '@opentelemetry/semantic-conventions'
-import {SimpleSpanProcessor, AlwaysOnSampler} from '@opentelemetry/sdk-trace-node'
-import {HttpInstrumentation} from "@opentelemetry/instrumentation-http";
+import {SimpleSpanProcessor} from '@opentelemetry/sdk-trace-node'
+
 import {SimpleLogRecordProcessor} from "@opentelemetry/sdk-logs";
 import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
+import {getNodeAutoInstrumentations} from "@opentelemetry/auto-instrumentations-node";
+import {PrismaInstrumentation} from "@prisma/instrumentation";
+
 
 const sdk = new NodeSDK({
   resource: new Resource({
@@ -18,9 +21,12 @@ const sdk = new NodeSDK({
     url: 'http://localhost:4318/v1/logs',
   })),
   instrumentations: [
-    new HttpInstrumentation()
+    getNodeAutoInstrumentations({
+      "@opentelemetry/instrumentation-fs": { enabled: false }
+
+    }),
+    new PrismaInstrumentation()
   ]
 })
-
 sdk.start()
 console.log('opentelemetry initialized')
